0452da212823ffdee08cd15993f4438e6645096b
Jose <jbcode.dev@proton.me>
2025-04-07 18:11:24 -0500

    Adding client information

---
+++ src/Challenge.java
@@ -0,0 +1,13 @@
+public class Challenge {
+    public static void main(String[] args) {
+        // Datos del cliente
+        String name = "Tony Stark";
+        String accountType = "Corriente";
+        double balance = 1599.99;
+        System.out.println("************************************");
+        System.out.println("\nNombre del cliente: " + name);
+        System.out.println("El tipo de cuenta es: " + accountType);
+        System.out.println("El saldo disponible es: $/." + balance);
+        System.out.println("\n************************************");
+    }
+}
